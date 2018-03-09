09e78d40a09258184917e7c8dd87b97305692c90
bugking <myway0710@naver.com>
2018-03-09 16:48:04 +0900

    main컬러 변경

--- loginAuto/Extensions/exUIColor.swift
+++ loginAuto/Extensions/exUIColor.swift
@@ -2,7 +2,7 @@
 import UIKit
 
 extension UIColor {
-    static var mainPink = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+    static var mainPink = #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1)
     static var pageTitle = UIColor(white: 0.2, alpha: 1)
     static var pageMessage = UIColor(white: 0.5, alpha: 1)
 }
