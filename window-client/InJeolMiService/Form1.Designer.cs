a2ea77b18c4b8e036521589d0414462fe75b2817
bugkingK <myway0710@naver.com>
2018-05-23 11:57:27 +0900

    window-client update

---
+++ window-client/InJeolMiService/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace InJeolMiService
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_className = new System.Windows.Forms.Button();
+            this.className = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "안녕하세요. 인절미 서비스입니다. ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "이 팝업창은 처음 실행시에만 작동합니다. ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(281, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "현재 작업 중이신 컴퓨터의 강의실을 입력해주세요.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(359, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "(예시: 6109, 6201, 3301, 3302, 나머지 문자는 입력되지 않습니다.)";
+            // 
+            // btn_className
+            // 
+            this.btn_className.Location = new System.Drawing.Point(234, 114);
+            this.btn_className.Name = "btn_className";
+            this.btn_className.Size = new System.Drawing.Size(75, 23);
+            this.btn_className.TabIndex = 0;
+            this.btn_className.Text = "확인";
+            this.btn_className.UseVisualStyleBackColor = true;
+            this.btn_className.Click += new System.EventHandler(this.btn_className_Click);
+            // 
+            // className
+            // 
+            this.className.Location = new System.Drawing.Point(30, 115);
+            this.className.Name = "className";
+            this.className.Size = new System.Drawing.Size(190, 21);
+            this.className.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 160);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.className);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_className);
+            this.Name = "Form1";
+            this.Text = "인절미 서비스";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_className;
+        private System.Windows.Forms.TextBox className;
+    }
+}
+
---
+++ window-client/InJeolMiService/Form1.cs
@@ -0,0 +1,267 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InJeolMiService
+{
+    public partial class Form1 : Form
+    {
+        static string macAddress;
+        static string server = "http://bug.lasel.kr/injeolmi/client/";
+        static string askExist = server + "askExist.php";
+        static string insertAddress = server + "insertAddress.php";
+        static string updateState = server + "updateState.php";
+        static string updateShutdown = server + "updateShutdown.php";
+       
+        Timer myTimer = new Timer();
+
+        public Form1()
+        {
+
+            if (!autoStart())
+            {
+                return;
+            }
+            //while (true)
+            //{
+            //    if (IsInternetConnected())
+            //    {
+            //        MessageBox.Show("인터넷이 연결되어 있습니다.");
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("연결이 안되어 있어요.");
+            //        System.Threading.Thread.Sleep(5000);
+            //    }
+            //}
+            
+            macAddress = NetworkInterface.GetAllNetworkInterfaces()[0].GetPhysicalAddress().ToString();
+            string resultAskExist = connect(askExist);
+            string[] resultExist = resultAskExist.Split('&');
+
+            if (resultExist[0].Equals("200")) { 
+                // 처음 시작      
+                InitializeComponent();
+            }
+            else if (resultExist[0].Equals("306"))
+            {
+                // 재접속
+                InitializeComponent();
+                Opacity = 0;
+                ShowInTaskbar = false;
+                string result = connect(updateShutdown, 1);
+                if (result.Equals("200"))
+                {
+                    // 5초마다 CheckStatus를 진행한다.
+                    timerSet();
+                } else
+                {
+                    MessageBox.Show("인터넷 연결상태가 불안정합니다.");
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("인터넷 연결상태가 불안정");
+                return;
+            }
+        }
+
+        
+
+        private void btn_className_Click(object sender, EventArgs e)
+        {
+            // 처음 실행 시 작동
+            string location = className.Text.ToString();
+            string resultinsertMac = connect(insertAddress, 1, location);
+            string[] resultInsert = resultinsertMac.Split('&');
+            if (resultInsert[0].Equals("200"))
+            {
+                // 업데이트 성공 시
+                Opacity = 0;
+                ShowInTaskbar = false;
+
+                // 5초마다 CheckStatus를 진행한다.
+                timerSet();
+            }
+            else
+            {
+                // 업데이트 실패 및 오류처리
+                MessageBox.Show(resultinsertMac);
+                return;
+            }
+        }
+
+        public void shutdown()
+        {
+            myTimer.Stop();
+            // 컴퓨터가 종료될거같으면 팝업창을 띄워서 취소시킬수 있는 메뉴를 보여준다.
+            Process.Start("shutdown.exe", "-s -t 120");
+            string resultUpdateState = connect(updateState, 0);
+            DialogResult resultShutdown = MessageBox.Show("120초 후에 컴퓨터가 자동 종료됩니다. 취소하시겠습니까?", "인절미 서비스", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            // state = 0으로 변경 
+            if (resultShutdown == DialogResult.Yes)
+            {
+                // state 와 shutdown을 모두 1로 바꿔야해 
+                Process.Start("shutdown.exe", "-a");
+                string result = connect(updateShutdown, 1);
+                
+                if (result.Equals("200"))
+                {
+                    // 5초마다 CheckStatus를 진행한다.
+                    myTimer.Start();
+                }
+                else
+                {
+                    //실패시 디비 shutdown을 3으로 변경하자 
+                    connect(updateState, 3);
+                    MessageBox.Show("프로그램 오류가 발생했습니다.");
+                }
+            }
+        }
+
+        public void timerSet()
+        {
+            myTimer.Interval = 5000;
+            myTimer.Tick += new EventHandler(CheckStatus);
+            myTimer.Enabled = true;
+        }
+
+        private void CheckStatus(object sender, EventArgs e)
+        {
+            string resultUpdateState = connect(updateState, 1);
+            if (resultUpdateState.Equals("202"))
+            {
+                shutdown();
+            }
+        }
+
+        static public string connect(string url, int state = 1, string location = "")
+        {
+            while (true)
+            {
+                if (IsInternetConnected())
+                {
+                    //MessageBox.Show("인터넷이 연결되어 있습니다.");
+                    break;
+                }
+                else
+                {
+                    //MessageBox.Show("연결이 안되어 있어요.");
+                    System.Threading.Thread.Sleep(5000);
+                }
+            }
+
+            try
+            {
+                string str_sendvalue = "macAddress=" + macAddress + "&state=" + state + "&location=" + location;
+                HttpWebRequest hwr = (HttpWebRequest)WebRequest.Create(url); // 객체를 생성한다.
+                hwr.Method = "POST"; // 포스트 방식으로 전달
+                hwr.ContentType = @"application/x-www-form-urlencoded";
+                byte[] buffer = Encoding.Default.GetBytes(str_sendvalue);
+                hwr.ContentLength = buffer.Length;
+                Stream sendStream = hwr.GetRequestStream(); // sendStream 을 생성한다.
+                sendStream.Write(buffer, 0, buffer.Length); // 데이터를 전송한다.34e
+                sendStream.Close(); // sendStream 을 종료한다.
+
+                HttpWebResponse wRespFirst = (HttpWebResponse)hwr.GetResponse();
+
+                // Response의 결과를 스트림을 생성합니다.
+                Stream respPostStream = wRespFirst.GetResponseStream();
+                StreamReader readerPost = new StreamReader(respPostStream, Encoding.Default);
+
+                // 생성한 스트림으로부터 string으로 변환합니다.
+                string resultPost = readerPost.ReadToEnd();
+                Console.WriteLine(resultPost);
+                        
+                return resultPost;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return "500";
+            }
+        }
+
+        static public bool IsInternetConnected()
+        {
+            const string NCSI_TEST_URL = "http://www.msftncsi.com/ncsi.txt";
+            const string NCSI_TEST_RESULT = "Microsoft NCSI";
+            const string NCSI_DNS = "dns.msftncsi.com";
+            const string NCSI_DNS_IP_ADDRESS = "131.107.255.255";
+
+            try
+            {
+                // Check NCSI test link
+                var webClient = new WebClient();
+                string result = webClient.DownloadString(NCSI_TEST_URL);
+                if (result != NCSI_TEST_RESULT)
+                {
+                    return false;
+                }
+
+                // Check NCSI DNS IP
+                var dnsHost = Dns.GetHostEntry(NCSI_DNS);
+                if (dnsHost.AddressList.Count() < 0 || dnsHost.AddressList[0].ToString() != NCSI_DNS_IP_ADDRESS)
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return false;
+            }
+
+            return true;
+        }
+
+        private static DateTime Delay(int MS)
+        {
+            DateTime ThisMoment = DateTime.Now;
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, MS);
+            DateTime AfterWards = ThisMoment.Add(duration);
+
+            while (AfterWards >= ThisMoment)
+            {
+                System.Windows.Forms.Application.DoEvents();
+                ThisMoment = DateTime.Now;
+            }
+
+            return DateTime.Now;
+        }
+
+        public bool autoStart()
+        {
+            try
+            {
+                RegistryKey strUpKey = Registry.CurrentUser.OpenSubKey(
+                                @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+                if (strUpKey.GetValue("InJeolMiService") == null)
+                {
+                    strUpKey.Close();
+                    strUpKey = Registry.CurrentUser.OpenSubKey(
+                                @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+                    strUpKey.SetValue("InJeolMiService", Application.ExecutablePath.ToString());
+                    //strUpKey.DeleteValue("InJeolMiService");
+                    
+                }
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("프로그램 오류가 발생했습니다. 프로그램을 삭제후 다시 설치해 주세요.");
+                return false;
+            }
+        }
+    }
+}
